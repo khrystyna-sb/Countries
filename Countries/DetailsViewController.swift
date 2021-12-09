837f490cfdd814f8716a83d7213b42ec8d6e840f
Khrystyna Matasova <>
2021-12-09 13:38:26 +0200

    update details screen

--- Countries/DetailsViewController.swift
+++ Countries/DetailsViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Countries
@@ -9,7 +10,24 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
-    let stackView: UIStackView = {
+    private enum Constants {
+        static let indent: CGFloat = 38.0
+        static let flagWidth: CGFloat = 127.0
+        static let flagHeigth: CGFloat = 75.0
+        static let backgroundColor = UIColor(red: 0.945, green: 0.945, blue: 0.945, alpha: 1)
+        static let topAnchorConstant: CGFloat = 55.0
+        static let leadingAnchorConstant: CGFloat = 34.0
+        static let trailingAnchorConstant: CGFloat = 112.0
+        static let bottomAnchorConstant: CGFloat = 58.0
+    }
+    
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let mainStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
@@ -17,43 +35,155 @@ class DetailsViewController: UIViewController {
         return stackView
     }()
     
+    let defaultView: UIView = {
+        let view = CountryTableViewHeader()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let headerView: DetailsVCHeader = {
+        let view = DetailsVCHeader()
+        view.contentMode = .scaleAspectFit
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+        view.backgroundColor = Constants.backgroundColor
+        }
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let flagImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     var country: CountriesApiQuery.Data.Country?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        createLabelsAndFlag()
-        setupStackView()
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            defaultImage()
+        }
+     
+        if country != nil {
+            defaultView.isHidden = true
+            createLabelsAndFlag()
+            configureFlag()
+            configureContents()
+        }
     }
     
-    private func setupStackView() {
-        view.addSubview(stackView)
+    private func defaultImage() {
+        view.addSubview(defaultView)
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -navigationItem.accessibilityFrame.size.height),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            defaultView.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.topAnchorConstant),
+            defaultView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingAnchorConstant),
+            defaultView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.trailingAnchorConstant),
+            defaultView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.bottomAnchorConstant)
         ])
     }
     
-    private func createLabelsAndFlag() {
+    private func configureFlag() {
+        
         guard let country = country else { return }
         
-        let flagImageView = UIImageView(image: UIImage(named: country.code.lowercased()))
-        flagImageView.contentMode = .scaleAspectFit
-        stackView.addArrangedSubview(flagImageView)
+        let layer = CALayer()
+        layer.cornerRadius = 5
+        flagImageView.layer.addSublayer(layer)
+        
+        let image = UIImage(named: country.code.lowercased())
+        flagImageView.image = image
+    }
+    
+    private func configureContents() {
+  
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            constraintsForIpad()
+        } else {
+            constraintsForIphone()
+        }
+    }
+    
+    private func constraintsForIpad() {
+        view.addSubview(containerView)
+        containerView.addSubview(headerView)
+        containerView.addSubview(mainStackView)
+        containerView.addSubview(flagImageView)
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.topAnchorConstant),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingAnchorConstant),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.trailingAnchorConstant),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.bottomAnchorConstant),
+            
+            headerView.heightAnchor.constraint(equalToConstant: PublicConstants.heightForHeader),
+            headerView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+            headerView.topAnchor.constraint(equalTo: containerView.topAnchor),
+
+            flagImageView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: Constants.indent),
+            flagImageView.widthAnchor.constraint(equalToConstant: Constants.flagWidth),
+            flagImageView.heightAnchor.constraint(equalToConstant: Constants.flagHeigth),
+            flagImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.indent),
+   
+            
+            mainStackView.topAnchor.constraint(equalTo: flagImageView.bottomAnchor, constant: Constants.indent),
+            mainStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -Constants.indent),
+            mainStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: Constants.indent),
+            mainStackView.bottomAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    
+    private func constraintsForIphone() {
+        view.addSubview(headerView)
+        view.addSubview(mainStackView)
+        view.addSubview(flagImageView)
+        
+        NSLayoutConstraint.activate([
+            
+            headerView.heightAnchor.constraint(equalToConstant: PublicConstants.heightForHeader),
+            headerView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -navigationItem.accessibilityFrame.size.height),
+            
+            flagImageView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: Constants.indent),
+            flagImageView.widthAnchor.constraint(equalToConstant: Constants.flagWidth),
+            flagImageView.heightAnchor.constraint(equalToConstant: Constants.flagHeigth),
+            flagImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.indent),
+            
+            mainStackView.topAnchor.constraint(equalTo: flagImageView.bottomAnchor, constant: Constants.indent),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.indent),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.indent),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+    }
+
+    
+    private func createLabelsAndFlag() {
+        
+        guard let country = country else { return }
+        
         var details: [String] = []
         
-        details.append(country.name)
+        let countryName = "Country\n" + country.name.uppercased()
+        details.append(countryName)
         if let capital = country.capital {
-            details.append(capital)
+            let capitalName = "Capital\n" + capital.uppercased()
+            details.append(capitalName)
         }
         
-        details.append(country.continent.name)
+        let continentName = "Region\n" + country.continent.name.uppercased()
+        details.append(continentName)
         
         if let currency = country.currency {
-            details.append(currency)
+            let currencyName = "Currency\n" + currency.uppercased()
+            details.append(currencyName)
         }
         
         var languageNames = ""
@@ -63,24 +193,57 @@ class DetailsViewController: UIViewController {
                 languageNames += " "
             }
         }
-        print(languageNames.isEmpty)
         if !languageNames.isEmpty {
-            details.append(languageNames)
+            let languagesName = "Ofisial languages\n" + languageNames.uppercased()
+            details.append(languagesName)
         }
         
-        details.append(country.phone)
+        let callingCode = "Calling code\n" + country.phone.uppercased()
+        details.append(callingCode)
         
-        var labels: [UILabel] = []
         for i in 0..<details.count {
             let label = UILabel()
             label.text = details[i]
-            
-            label.textAlignment = .center
+            label.textAlignment = .left
             label.numberOfLines = 2
-            labels.append(label)
-            stackView.addArrangedSubview(label)
+            
+            
+            let horysontalStsckView = UIStackView()
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFit
+            
+            if i % 2 == 0 {
+                let image = UIImage(named: "ellipse1")
+                imageView.image = image
+            } else {
+                let image = UIImage(named: "ellipse2")
+                imageView.image = image
+            }
+            
+            let vectorImage = UIImageView()
+            vectorImage.image = UIImage(named: "vector")
+            vectorImage.contentMode = .scaleAspectFit
+            
+            let verticalStackView = UIStackView()
+            verticalStackView.axis = .vertical
+            verticalStackView.alignment = .leading
+            verticalStackView.distribution = .fillEqually
+            verticalStackView.spacing = 10
+            verticalStackView.addArrangedSubview(imageView)
+            if i < details.count - 1 {
+                verticalStackView.addArrangedSubview(vectorImage)
+            } else {
+                verticalStackView.addArrangedSubview(UIImageView())
+            }
+            
+            horysontalStsckView.addArrangedSubview(verticalStackView)
+            horysontalStsckView.addArrangedSubview(label)
+            horysontalStsckView.spacing = 20
+            horysontalStsckView.alignment = .top
+            horysontalStsckView.distribution = .fillProportionally
+            
+            mainStackView.addArrangedSubview(horysontalStsckView)
         }
     }
 }
-
 
