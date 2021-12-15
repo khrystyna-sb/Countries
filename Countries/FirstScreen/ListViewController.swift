b66abc96db3bb1cfb8b310747cd5f40cf34a74c0
Vova Seredovych <66167568+Globalika@users.noreply.github.com>
2021-12-15 14:16:09 +0200

    Search and filter component (#13)
    * added refresh controll
    
    * added refresh function
    
    * added refresh controller setup
    
    * added search controller
    
    * added filteredCountries functions
    
    * setup search controller
    
    * updated tableview to match filter
    
    * moved NA to constants
    
    * moved searchbar placeholder to constants
    
    * removed scope related code
    
    * changed numbers of row func
    
    * removed objc from loaddata func
    
    * resolved @khrystyna-sb comments
    
    * removed filter check
    
    * fixed data sending bug
    
    * changed scroll beheavior
    
    * added TODO
    
    * fixed typo

--- Countries/FirstScreen/ListViewController.swift
+++ Countries/FirstScreen/ListViewController.swift
@@ -7,38 +7,57 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
+class ListViewController: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating {
     
     private enum Constants {
         static let heightForRow: CGFloat = 179.0
+        static let notAplicableField = "NA"
+        static let searchBarPlaceHolder = "Find Countries"
     }
     
     var countries: [CountriesApiQuery.Data.Country] = []
-    
-    let refrechControl: UIRefreshControl = {
-        let refrechControll = UIRefreshControl()
-        return refrechControll
+    var filteredCountries: [CountriesApiQuery.Data.Country] = []
+    let tableRefreshControl = UIRefreshControl()
+
+    let searchController: UISearchController = {
+        let controller = UISearchController()
+        controller.searchBar.searchBarStyle = .minimal
+        controller.hidesNavigationBarDuringPresentation = false
+        controller.searchBar.placeholder = Constants.searchBarPlaceHolder
+        return controller
     }()
+
+    @objc private func refresh(sender: UIRefreshControl) {
+        self.searchController.searchBar.text = ""
+        loadData(sender: sender)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         registerTableView()
         setupRefreshController()
         setupNavigationItem()
+        setUpSearchController()
         loadData()
     }
     
     func setupRefreshController() {
-        tableView.refreshControl = refrechControl
-        tableView.refreshControl?.addTarget(self, action: #selector(loadData(sender: )), for: .valueChanged)
+        tableView.refreshControl = tableRefreshControl
+        tableView.refreshControl?.addTarget(self, action: #selector(refresh(sender: )), for: .valueChanged)
     }
     
     func setupNavigationItem() {
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController = searchController
         self.navigationItem.title = "Country list"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-    
+
+    func setUpSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+    }
+
     private func registerTableView() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -48,21 +67,20 @@ class ListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return countries.count
+        return filteredCountries.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CountryTableViewCell.identifier, for: indexPath) as? CountryTableViewCell else {return UITableViewCell()}
-        let country = countries[indexPath.row]
+        let country = filteredCountries[indexPath.row]
         cell.configure(counrty: country)
-        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let country = countries[indexPath.row]
+        let country = filteredCountries[indexPath.row]
         let detailsVC = DetailsViewController()
         detailsVC.country = country
         
@@ -82,11 +100,33 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return (UIDevice.current.userInterfaceIdiom == .phone) ? PublicConstants.heightForHeader : 0
     }
+
+    func filterContentForSearchText(searchText: String) {
+        filteredCountries = countries.filter({
+            $0.continent.name.lowercased().contains(searchText.lowercased()) ||
+            ($0.capital ?? Constants.notAplicableField).lowercased().contains(searchText.lowercased()) ||
+            $0.name.lowercased().contains(searchText.lowercased())
+        })
+        //  TODO change filter condition
+        if isSearchBarEmpty() { filteredCountries = countries }
+        tableView.reloadData()
+    }
+
+    func isSearchBarEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text ?? "")
+    }
+
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchText: searchBar.text ?? "")
+    }
 }
 
 extension ListViewController {
-    @objc func loadData(sender: UIRefreshControl? = nil) {
-        
+    func loadData(sender: UIRefreshControl? = nil) {
         let query = CountriesApiQuery()
         guard let client = Apollo.shared.client else { return }
         client.fetch(query: query) { result in
@@ -95,6 +135,7 @@ extension ListViewController {
             case .success(let graphQLResult):
                 if let countries = graphQLResult.data?.countries.compactMap({ $0 }) {
                     self.countries = countries
+                    self.filteredCountries = countries
                     self.tableView.reloadData()
                 }
                 
