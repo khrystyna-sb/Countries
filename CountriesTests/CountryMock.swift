41ff33ff747f1f88ffa2875a71cbc6ca15493850
Khrystyna Matasova <>
2021-12-21 15:36:32 +0200

    create mock

---
+++ CountriesTests/CountryMock.swift
@@ -0,0 +1,18 @@
+//
+//  CountryMock.swift
+//  CountriesTests
+//
+//  Created by Khrystyna Matasova on 21.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class CountryMock {
+    var name = "Norway"
+    var capital = "Oslo"
+    var region = "Europe"
+    var currency = "NOK"
+    var languages = ["Norvegian"]
+    var callingCode = "47"
+}
