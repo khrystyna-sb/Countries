cbea9908bd8775a20cd26bad222531ecb574d969
Khrystyna Matasova <>
2021-12-21 16:57:21 +0200

    add one test for containSearchText

--- CountriesTests/CountriesTests.swift
+++ CountriesTests/CountriesTests.swift
@@ -10,24 +10,10 @@ import XCTest
 
 class CountriesTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testContainSearchText() throws {
+        let searchText = "d"
+        let string = "AnDorra"
+        let result = string.containSearchText(searchText: searchText)
+        XCTAssertEqual(result, true)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
--- CountriesTests/CountryMock.swift
+++ CountriesTests/CountryMock.swift
@@ -8,11 +8,55 @@
 import Foundation
 import UIKit
 
-class CountryMock {
+class CountriesMock {
+    var norway = Norway()
+    var ukraine = Ukraine()
+    
+    func getCountries() -> [CountryMock] {
+        return [norway, ukraine]
+    }
+}
+
+protocol CountryMock {
+    var name: String { get set }
+    var capital: String { get set }
+    var continent: String { get set }
+}
+
+class Norway: CountryMock {
     var name = "Norway"
     var capital = "Oslo"
-    var region = "Europe"
-    var currency = "NOK"
-    var languages = ["Norvegian"]
-    var callingCode = "47"
+    var continent = "Europe"
 }
+
+class Ukraine: CountryMock {
+    var name = "Ukraine"
+    var capital = "Kyiv"
+    var continent = "Europe"
+}
+
+
+extension Array where Element == [CountryMock]  {
+    typealias Country = [CountryMock]
+    private struct Constants {
+        static let notApplicableField: String = "N-A"
+    }
+
+    func filterByName(searchText: String) -> [Country] {
+        return filter { $0.name.containSearchText(searchText: searchText) }
+    }
+    func filterByCapital(searchText: String) -> [Country] {
+        return filter { ($0.capital ?? Constants.notApplicableField).containSearchText(searchText: searchText) }
+    }
+    func filterByContinent(searchText: String) -> [Country] {
+        return filter { $0.continent.name.containSearchText(searchText: searchText) }
+    }
+//    func filterCounties(searchText: String) -> [Country] {
+//        return filter {
+//            $0.name.containSearchText(searchText: searchText) ||
+//            ($0.capital ?? Constants.notApplicableField).containSearchText(searchText: searchText) ||
+//            $0.continent.name.containSearchText(searchText: searchText)
+//        }
+//    }
+}
+
