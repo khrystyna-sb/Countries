5e40cc4a8e1bb79eb03d5e7c036a43ff260290ff
Khrystyna Matasova <>
2022-01-04 15:24:41 +0200

    moved MockNetworkManager to a separate file

---
+++ Countries/MockNetworkManager.swift
@@ -0,0 +1,21 @@
+//
+//  MockNetworkManager.swift
+//  Countries
+//
+//  Created by Khrystyna Matasova on 04.01.2022.
+//
+
+import Foundation
+
+class MockNetworkManager: NetworkManagerProtocol {
+    
+    func getCountry() -> CountriesApiQuery.Data.Country {
+        let continent = CountriesApiQuery.Data.Country.Continent(name: "testcontinent")
+        let language = CountriesApiQuery.Data.Country.Language(name: "testlanguage")
+        return CountriesApiQuery.Data.Country(code: "UA", name: "testname", capital: "testcapital", currency: "testcurrency", phone: "testphone", continent: continent, languages: [language])
+    }
+    
+    func getCountries(completion: @escaping (Result<[CountriesApiQuery.Data.Country]?, Error>) -> Void) {
+        completion(Result.success([getCountry(), getCountry()]))
+    }
+}
